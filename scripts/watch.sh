7078ea77eff31a7c718b2e3e35b6f02d0699b1a4
Colbyn Wadman <hello@colbyn.com>
2020-10-01 08:54:50 -0600

    misc updates

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,4 +2,4 @@ set -e
 
 # watchexec --exts adoc,css,yml --ignore output/ -- './scripts/build.sh && echo reloaded && open --background output/main.pdf'
 
-watchexec --exts adoc,css,yml,tex --ignore output/ -- './scripts/build.sh && echo reloaded'
+watchexec --exts adoc,css,yml,tex,txt --ignore output/ -- './scripts/build.sh && echo reloaded'
